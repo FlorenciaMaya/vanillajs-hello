e5186889fc4ad8aaac5b42d036cb14a9fad77902
Florencia <florenciamayaumpierrez@gmail.com>
2024-09-16 23:08:45 +0000

    Mi generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,48 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Un otaku",
+    "Hulk",
+    "Homero Simpson",
+    "Un aliems",
+    "Michael Jackson",
+    "El profe"
+  ];
+  let action = [
+    "tiró R en",
+    "aplastó",
+    "saltó sobre",
+    "hackeó",
+    "vomitó en",
+    "explotó"
+  ];
+  let what = [
+    "mi tarea",
+    "mi consola",
+    "una canoa",
+    "la mesa",
+    "mis conocimientos",
+    "el codigo"
+  ];
+  let when = [
+    "durante la misa",
+    "mientras bailaba con Cristian",
+    "en la mañana",
+    "mientras estudiaba",
+    "mientras jugaba LOL con Samuel",
+    "mientras tomaba mate con Ceci"
+  ];
+
+  function getRandom(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber];
+  }
+
+  let excusa = `
+  ${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}
+`;
+
+  console.log(excusa);
+  document.getElementById("excuse").innerHTML = excusa;
 };
